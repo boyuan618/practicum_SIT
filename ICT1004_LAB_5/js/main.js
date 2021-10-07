d47591bc10f73a0577008f86d75103fa253b4189
ningmeng618 <gu.boyuan@dhs.sg>
2021-10-07 12:39:08 +0800

    Added ICT1004 LAB 5

---
+++ ICT1004_LAB_5/js/main.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    var imgs = document.getElementsByClassName("img-small")
+    for (var i=0;i<imgs.length;i++) {
+        imgs[i].addEventListener("click", function(img) {
+            var animal = img.target.alt;
+            console.log(animal);
+            console.log(img);
+            var span = document.createElement("span");
+            span.setAttribute("class", "img-popup")
+            span.setAttribute("id", "img-popup")
+            var img = document.createElement("img")
+            img.setAttribute("src", "images/"+animal+"_small.jpg");
+            img.setAttribute("id", "popup")
+            img.setAttribute("class", "popup")
+            span.appendChild(img);
+            const element = document.getElementById(animal);
+            span.addEventListener("click", remove_img);
+            element.appendChild(span);
+        });
+        
+    }
+
+});
+
+function remove_img() {
+    var span = document.getElementById("img-popup");
+    span.remove();
+};
